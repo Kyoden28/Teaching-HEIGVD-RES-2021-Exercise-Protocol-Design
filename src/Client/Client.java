d514a3f1e0d18e2862c73eb610a568840e745f45
Christian Gomes <35340658+Kyoden28@users.noreply.github.com>
2021-04-11 17:47:38 +0200

    Add Client Side Implementation

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,102 @@
 package Client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * This class implements a simple client
+ *
+ * Laboratoire Protocol
+ *
+ * @date   11.04.2021
+ * @author Christian Gomes & Johann Werkle
+ *
+ */
 
 public class Client {
+
+    private static int PORT = 4000;
+    Socket clientSocket = null;
+    BufferedReader in = null;
+    PrintWriter out = null;
+    boolean isConnected = false;
+
+
+    public static void main(String[] args) {
+
+        try {
+            new Client().connect("localhost", PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    class Listener implements Runnable {
+
+        @Override
+        public void run() {
+
+            String response = "";
+            try {
+                while ((isConnected && (response = in.readLine()) != null)) {
+                    System.out.println(response);
+                }
+            } catch (IOException e) {
+                System.out.println(e.getStackTrace());
+                isConnected = false;
+            }
+        }
+    }
+
+
+    public void connect(String serverAdress, int portServer) throws IOException {
+
+        //Connection on Server
+        try{
+            //Localhost dans le cas du laboratoire
+            clientSocket = new Socket(serverAdress,portServer);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream());
+            isConnected = true;
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Listener()).start();
+
+        out.println("CONNECT");
+        out.flush();
+
+        calculate();
+
+    }
+
+    public void calculate() throws IOException {
+
+        //Read Input User
+        BufferedReader standardInput =new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while((line = standardInput.readLine()) != "QUIT"){
+
+            out.println(line);
+            out.flush();
+        }
+
+        disconnect();
+
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        out.println("QUIT");
+    }
+
 }
